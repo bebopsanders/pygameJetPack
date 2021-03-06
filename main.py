6c29be7e6282ffb689c429e163004f49aeb35bf9
Tyson Van Buren <bebopsanders@gmail.com>
2021-03-06 13:23:28 -0700

    Added main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame as pg
+
+WIDTH = 1200
+HEIGHT = 600
+
+pg.init()
+screen = pg.display.set_mode((W,H))
+clock = pg.time.Clock()
+
+all_sprites = pg.sprite.Group()
+
+running = True
+while running:
+    clock.tick(FPS)
+    screen.fill((0,0,0))
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+    all_sprites.update()
+    all_sprites.draw(screen)
+    pg.display.flip()
